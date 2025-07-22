30bd7f999932f2fb42df90f40fa502726c471834
mijin <mnmrtwg@gmail.com>
2025-07-22 17:06:56 +0900

    design: home 화면 레이아웃 생성

---
+++ backend/src/components/common/Header.tsx
@@ -0,0 +1,11 @@
+type HeaderProps = {};
+
+const Header = ({}: HeaderProps) => {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ backend/src/components/domain/artist/ArtistCard.tsx
@@ -0,0 +1,11 @@
+type ArtistCardProps = {};
+
+const ArtistCard = ({}: ArtistCardProps) => {
+  return (
+    <div>
+      <h3>ArtistCard</h3>
+    </div>
+  );
+};
+
+export default ArtistCard;
