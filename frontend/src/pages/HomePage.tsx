90db9dee82811675164bae754f4f1260b059719c
mijin <mnmrtwg@gmail.com>
2025-07-22 17:06:56 +0900

    design: home 화면 레이아웃 생성

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,13 +1,32 @@
-type AppProps = {
-  message: string;
-};
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
+import VideoCard from "../components/domain/video/VideoCard";
+import ArtistCard from "../components/domain/artist/ArtistCard";
 
-const App = ({ message }: AppProps) => {
+const HomePage = ({}: HomePageProps) => {
   return (
     <div>
-      <h1>{message}</h1>
+      <div>
+        <Header />
+      </div>
+      <div>이미지 넣을 거임</div>
+      <div>
+        <h1>지금 핫 한 방 !!!!!</h1>
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+      </div>
+      <div>
+        <h1>추천 아티스트 !!!!</h1>
+        <ArtistCard />
+        <ArtistCard />
+        <ArtistCard />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
 
-export default App;
+export default HomePage;
