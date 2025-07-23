75d482be3eb08c5ecc0d664bde87fb183678332c
wnsqja7545 <wnsqja7545@gmail.com>
2025-07-23 21:02:05 +0900

    feat : JWT 인증 추가

--- frontend/src/main/java/com/a404/duckonback/config/SecurityConfig.java
+++ frontend/src/main/java/com/a404/duckonback/config/SecurityConfig.java
@@ -1,16 +1,29 @@
 package com.a404.duckonback.config;
 
+import com.a404.duckonback.Filter.JWTFilter;
+import com.a404.duckonback.service.UserServiceImpl;
+import com.a404.duckonback.util.JWTUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+
+
+    private final JWTFilter jwtFilter;
+
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -20,11 +33,15 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/signup", "/api/auth/login").permitAll()
+                        .requestMatchers("/").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
                 )
-                .httpBasic(withDefaults());
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
---
+++ frontend/src/main/java/com/a404/duckonback/util/JWTUtil.java
@@ -0,0 +1,65 @@
+package com.a404.duckonback.util;
+
+import com.a404.duckonback.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.UUID;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.Claims;
+
+import javax.crypto.SecretKey;
+
+
+@Component
+public class JWTUtil {
+
+    private final Key key = Keys.hmacShaKeyFor("DuckOnSecretKeyMustBeAtLeast32ByteLong".getBytes(StandardCharsets.UTF_8));
+
+//    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256); // ( 임시 ) 서버 실행마다 바뀜 -> 기존 토큰 무효화됨
+    private final long accessTokenExpiration = 1000L * 60 * 60 * 24 * 30; // ( 임시 ) 30일
+    private final long refreshTokenExpiration = 1000L * 60 * 60 * 24 * 30; // ( 임시 ) 30일
+
+    public String generateAccessToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getUserId())
+                .claim("role", user.getRole())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpiration))
+                .signWith(key)
+                .compact();
+    }
+    public String generateRefreshToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getUserId())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenExpiration))
+                .signWith(key)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith((SecretKey) key).build().parseSignedClaims(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
