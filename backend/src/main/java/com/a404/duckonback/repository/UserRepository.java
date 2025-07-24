a969743d843b11fc5f9acc9cc2497d9c3b803413
4kmj54321 <4kmj54321@naver.com>
2025-07-24 17:14:14 +0900

    feat: add OAuth2 client configuration for Google, Kakao, and Naver

--- backend/src/main/java/com/a404/duckonback/repository/UserRepository.java
+++ backend/src/main/java/com/a404/duckonback/repository/UserRepository.java
@@ -5,8 +5,6 @@ import com.a404.duckonback.enums.SocialProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.ScopedValue;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     User findByEmail(String email);
