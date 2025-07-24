2af6198e00ee82217513b0461b29d969f0e73172
4kmj54321 <4kmj54321@naver.com>
2025-07-24 16:34:26 +0900

    feat: enhance security configuration with OAuth2 success and failure handlers

--- backend/src/main/java/com/a404/duckonback/config/SecurityConfig.java
+++ backend/src/main/java/com/a404/duckonback/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.a404.duckonback.config;
 
 import com.a404.duckonback.filter.JWTFilter;
+import com.a404.duckonback.oauth.handler.OAuth2FailureHandler;
+import com.a404.duckonback.oauth.handler.OAuth2SuccessHandler;
+import com.a404.duckonback.oauth.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,10 +18,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-
-
     private final JWTFilter jwtFilter;
-
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final OAuth2FailureHandler oAuth2FailureHandler; // 간단히 401 리다이렉트 처리용
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -33,12 +36,21 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/signup", "/api/auth/login").permitAll()
-                        .requestMatchers("/api/auth/refresh").permitAll()
+                        .requestMatchers(
+                                "/api/auth/signup",
+                                "/api/auth/login",
+                                "/oauth2/**"
+                        ).permitAll()
                         .requestMatchers("/").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                .oauth2Login(oauth -> oauth
+                        .userInfoEndpoint(u -> u.userService(customOAuth2UserService))
+                        .successHandler(oAuth2SuccessHandler)
+                        .failureHandler(oAuth2FailureHandler)
+                );
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
