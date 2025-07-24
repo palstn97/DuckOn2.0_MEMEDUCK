b64c0d993747e37d302aea14d165e32b7af43a2d
yejin <qja086351@naver.com>
2025-07-24 18:00:12 +0900

    feat: 회원 정보 조회 detail api 구현
    roomList 및 artistList 관련 추가 구현 필요
    uuid 부분 수정 필요

--- backend/src/main/java/com/a404/duckonback/service/UserService.java
+++ backend/src/main/java/com/a404/duckonback/service/UserService.java
@@ -1,5 +1,6 @@
 package com.a404.duckonback.service;
 
+import com.a404.duckonback.dto.UserInfoResponseDTO;
 import com.a404.duckonback.entity.User;
 
 public interface UserService {
@@ -9,5 +10,5 @@ public interface UserService {
     boolean isEmailDuplicate(String email);
     boolean isUserIdDuplicate(String userId);
     boolean isNicknameDuplicate(String nickname);
-
+    UserInfoResponseDTO getUserInfo(String accessToken);
 }
