facdb63af7f0d459f5de79e315d3ace853cc9ea6
minjoon-98 <4kmj54321@gmail.com>
2025-07-24 10:26:29 +0900

    chore: 환경변수 분리 및 .env 적용을 위한 설정 리팩토링
    - DB 및 JWT 설정을 .env 파일 기반으로 외부 분리
    - application.properties에서 환경변수 ${...} 형식으로 치환
    - ServiceProperties 클래스 기반 설정 적용 유지
    - JWTUtil에서 key 초기화를 @PostConstruct로 변경
    - .gitignore에 .env, env.properties 파일 추가

---
+++ backend/src/main/java/com/a404/duckonback/config/ServiceProperties.java
@@ -0,0 +1,28 @@
+package com.a404.duckonback.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class ServiceProperties {
+
+    @Value("${JWT_SECRET_KEY}")
+    private String jwtSecret;
+
+    @Value("${JWT_ACCESS_EXPIRATION}")
+    private long accessTokenExpiration;
+
+    @Value("${JWT_REFRESH_EXPIRATION}")
+    private long refreshTokenExpiration;
+
+    @Value("${SPRING_DATASOURCE_URL}")
+    private String dbUrl;
+
+    @Value("${SPRING_DATASOURCE_USERNAME}")
+    private String dbUsername;
+
+    @Value("${SPRING_DATASOURCE_PASSWORD}")
+    private String dbPassword;
+}
