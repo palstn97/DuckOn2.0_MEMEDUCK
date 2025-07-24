4116597ebab6eb1d9e2a29d926ce7a3ce7dbcd5a
yejin <qja086351@naver.com>
2025-07-24 18:00:12 +0900

    feat: 회원 정보 조회 detail api 구현
    roomList 및 artistList 관련 추가 구현 필요
    uuid 부분 수정 필요

--- backend/BE/src/main/java/com/a404/duckonback/service/AuthService.java
+++ backend/BE/src/main/java/com/a404/duckonback/service/AuthService.java
@@ -3,11 +3,11 @@ package com.a404.duckonback.service;
 import com.a404.duckonback.dto.LoginRequestDTO;
 import com.a404.duckonback.dto.LoginResponseDTO;
 import com.a404.duckonback.dto.SignupRequestDTO;
+import com.a404.duckonback.dto.UserInfoResponseDTO;
 import org.springframework.http.ResponseEntity;
 
 public interface AuthService {
     LoginResponseDTO login(LoginRequestDTO loginRequest);
     ResponseEntity<?> signup(SignupRequestDTO dto);
     String refreshAccessToken(String refreshToken);
-
 }
--- backend/BE/src/main/java/com/a404/duckonback/service/UserServiceImpl.java
+++ backend/BE/src/main/java/com/a404/duckonback/service/UserServiceImpl.java
@@ -1,10 +1,17 @@
 package com.a404.duckonback.service;
 
+import com.a404.duckonback.dto.RoomDTO;
 import com.a404.duckonback.dto.SignupRequestDTO;
+import com.a404.duckonback.dto.UserInfoResponseDTO;
+import com.a404.duckonback.entity.Room;
 import com.a404.duckonback.entity.User;
 import com.a404.duckonback.enums.UserRole;
+import com.a404.duckonback.exception.CustomException;
+import com.a404.duckonback.repository.RoomRepository;
 import com.a404.duckonback.repository.UserRepository;
+import com.a404.duckonback.util.JWTUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,6 +27,8 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final JWTUtil jwtUtil;
+    private final RoomRepository roomRepository;
 
     public User findByEmail(String email) {
         return userRepository.findByEmail(email);
@@ -42,6 +51,37 @@ public class UserServiceImpl implements UserService {
 
     public boolean isNicknameDuplicate(String nickname) {
         return userRepository.existsByNickname(nickname);
+    }
+
+    public UserInfoResponseDTO getUserInfo(String accessToken) {
+        String userId = jwtUtil.getClaims(accessToken).getSubject();
+
+        User user = userRepository.findByUserId(userId);
+
+        if(user == null){
+            throw new CustomException("사용자를 찾을 수 없습니다", HttpStatus.UNAUTHORIZED);
+        }
+
+//        List<Room> rooms = roomRepository.findByUserId(userId);
+        List<RoomDTO> rooms = null;
+
+
+        return UserInfoResponseDTO.builder()
+                .userId(user.getUserId())
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .role(user.getRole().toString())
+                .language(user.getLanguage())
+                .imgUrl(user.getImgUrl())
+                .artistList(user.getArtistFollows().stream().map(
+                        artistFollow -> artistFollow.getArtist().getArtistId()
+                ).toList())
+                .followingCount(user.getFollowing().size())
+                .followerCount(user.getFollowers().size())
+                .bannedTill(null) // join
+                .password(user.getPassword())
+                .roomList(rooms) // 개발 필요
+                .build();
     }
 
 }
