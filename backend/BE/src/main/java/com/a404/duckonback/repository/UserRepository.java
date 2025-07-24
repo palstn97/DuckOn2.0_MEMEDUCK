9970a24acaa386aa7246ec92def4c35f8d2ec94a
4kmj54321 <4kmj54321@naver.com>
2025-07-24 15:57:20 +0900

    feat: implement OAuth2 success handler and user principal for authentication

--- backend/BE/src/main/java/com/a404/duckonback/repository/UserRepository.java
+++ backend/BE/src/main/java/com/a404/duckonback/repository/UserRepository.java
@@ -1,12 +1,17 @@
 package com.a404.duckonback.repository;
 
 import com.a404.duckonback.entity.User;
+import com.a404.duckonback.enums.SocialProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.lang.ScopedValue;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     User findByEmail(String email);
     User findByUserId(String userId);
     User findByUuid(String uuid);
+
+    User findByProviderAndProviderId(SocialProvider provider, String providerId);
 }
