be121fd5a9e32a033f7464fd7e7324608b819558
yejin <qja086351@naver.com>
2025-07-25 15:19:17 +0900

    fix: 개발 용 에러 출력 코드 추가

--- backend/src/main/java/com/a404/duckonback/controller/UserController.java
+++ backend/src/main/java/com/a404/duckonback/controller/UserController.java
@@ -30,6 +30,9 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body(Map.of("message", "Access Token이 유효하지 않거나 누락되었습니다."));
         } catch (Exception e) {
+            // 개발 중 사용
+            System.out.println("Error in getMyInfo: " + e.getMessage());
+            e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(Map.of("message", "서버 내부 오류가 발생했습니다. 잠시 후 다시 시도해주세요."));
         }
