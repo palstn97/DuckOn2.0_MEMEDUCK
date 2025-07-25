58d66280042384ca3844e842f547218d849af6f0
mijin <alwlsdlqslek3@gmail.com>
2025-07-25 13:18:08 +0900

    feat: 회원가입 페이지 통신 구현

--- backend/FE/src/components/common/InputField.tsx
+++ backend/FE/src/components/common/InputField.tsx
@@ -7,6 +7,9 @@ type InputFieldProps = {
   placeholder: string;
   helperText?: string;
   icon: React.ReactNode;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  error?: string;
 };
 
 const InputField = ({
@@ -16,9 +19,13 @@ const InputField = ({
   placeholder,
   helperText,
   icon,
+  value,
+  onChange,
+  error,
 }: InputFieldProps) => {
   return (
     <div>
+      {/* 필드 설명 영역 */}
       <div className="flex justify-between items-center mb-2">
         <label htmlFor={id} className="text-gray-700 text-sm font-medium">
           {label}*
@@ -29,6 +36,7 @@ const InputField = ({
           </span>
         )}
       </div>
+      {/* 인풋 영역 */}
       <div className="relative">
         <div className="absolute inset-y-0 left-0 pl-3.5 flex items-center pointer-events-none">
           {icon}
@@ -36,9 +44,16 @@ const InputField = ({
         <input
           type={type}
           id={id}
+          name={id}
+          value={value}
+          onChange={onChange}
           className="w-full h-12 pl-12 pr-4 py-3 bg-white rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500"
           placeholder={placeholder}
         />
+      </div>
+      {/* 에러 메시지 영역 */}
+      <div className="min-h-[1rem] mt-1">
+        {error && <p className="text-xs text-red-500 leading-tight">{error}</p>}
       </div>
     </div>
   );
