a3f00e479f589f6deceea6333a6fa617f1e8e07a
Minsoo Park <palstn0624@gmail.com>
2025-07-26 13:45:23 +0900

    feat: 마이페이지 생성 및 프로필 수정 시 비밀번호 확인 및 팝업 처리 구현

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,5 +1,6 @@
 import { type User } from "../../types";
 import Button from "./Button";
+import { useNavigate } from "react-router-dom";
 
 type HeaderProps = {
   user: User | null;
@@ -8,16 +9,18 @@ type HeaderProps = {
   onLogout: () => void;
 };
 
+
 /* 
-  name : Header
-  summary : User 객체의 존재 여부에 따라 버튼이 구성요소가 달라짐
-  props
-    - user : 로그인 여부를 판단하는 User 객체 (현재 임시 index.ts에 User 타입 사용)
-    - onLogin() : 로그인 페이지로 이동
-    - onSignup() : 회원가입 페이지로 이동
-    - onLogout() : 로그아웃 페이지로 이동 
+name : Header
+summary : User 객체의 존재 여부에 따라 버튼이 구성요소가 달라짐
+props
+- user : 로그인 여부를 판단하는 User 객체 (현재 임시 index.ts에 User 타입 사용)
+- onLogin() : 로그인 페이지로 이동
+- onSignup() : 회원가입 페이지로 이동
+- onLogout() : 로그아웃 페이지로 이동 
 */
 const Header = ({ user, onLogin, onSignup, onLogout }: HeaderProps) => {
+  const navigate = useNavigate()
   return (
     <header className="w-full bg-white border-b border-gray-200">
       <nav className="max-w-7xl h-16 mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
@@ -31,7 +34,12 @@ const Header = ({ user, onLogin, onSignup, onLogout }: HeaderProps) => {
           {user ? (
             // 로그인 상태일 때
             <>
-              <span className="text-sm font-medium">{user.nickname} 님</span>
+              <span 
+                onClick={() => navigate('/mypage')}
+                className="text-sm font-medium text-gray-700 hover:text-purple-600 hover:underline transition-colors duration-200 cursor-pointer"
+              >
+                {user.nickname} 님
+              </span>
               <Button onClick={onLogout} variant="secondary">
                 로그아웃
               </Button>
