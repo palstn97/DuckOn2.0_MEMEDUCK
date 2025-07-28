95af776223bbbc5130c4232dbe7dc7fcaab1c193
wnsqja7545 <wnsqja7545@gmail.com>
2025-07-28 13:39:59 +0900

    refactor: Filter가 accessToken 인증후 user정보 저장 -> Controller : 저장된 정보 가져오기만

--- frontend/BE/src/main/java/com/a404/duckonback/controller/UserController.java
+++ frontend/BE/src/main/java/com/a404/duckonback/controller/UserController.java
@@ -2,11 +2,13 @@ package com.a404.duckonback.controller;
 
 import com.a404.duckonback.dto.UserDetailInfoResponseDTO;
 import com.a404.duckonback.exception.CustomException;
+import com.a404.duckonback.filter.CustomUserDetails;
 import com.a404.duckonback.service.UserService;
 import com.a404.duckonback.util.JWTUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -19,27 +21,33 @@ public class UserController {
     private final UserService userService;
     private final JWTUtil jwtUtil;
 
+//    @GetMapping("/me")
+//    public ResponseEntity<?> getMyInfo(@RequestHeader("Authorization") String authorization) {
+//        try {
+//            UserDetailInfoResponseDTO userInfo = userService.getUserDetailInfo(authorization);
+//            return ResponseEntity.ok(userInfo);
+//        } catch (CustomException e) {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+//                    .body(Map.of("message", "Access Token이 유효하지 않거나 누락되었습니다."));
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                    .body(Map.of("message", "서버 내부 오류가 발생했습니다. 잠시 후 다시 시도해주세요."));
+//        }
+//    }
+
     @GetMapping("/me")
-    public ResponseEntity<?> getMyInfo(@RequestHeader("Authorization") String authorization) {
-        try {
-            UserDetailInfoResponseDTO userInfo = userService.getUserDetailInfo(authorization);
-            return ResponseEntity.ok(userInfo);
-        } catch (CustomException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(Map.of("message", "Access Token이 유효하지 않거나 누락되었습니다."));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(Map.of("message", "서버 내부 오류가 발생했습니다. 잠시 후 다시 시도해주세요."));
-        }
+    public ResponseEntity<?> getMyInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseEntity.ok(userService.getUserDetailInfo(userDetails.getUser().getUserId()));
     }
+
 
 
     @DeleteMapping("/me")
     public ResponseEntity<?> deleteUser(
-            @RequestHeader("Authorization") String accessTokenHeader,
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestHeader("X-Refresh-Token") String refreshTokenHeader) {
 
-        userService.deleteUser(accessTokenHeader, refreshTokenHeader);
+        userService.deleteUser(userDetails.getUser(), refreshTokenHeader);
         return ResponseEntity.ok(Map.of("message", "회원탈퇴가 완료되었습니다."));
     }
 
