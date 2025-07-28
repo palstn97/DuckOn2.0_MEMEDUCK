f824d3339723245199d65547e02eb3f095cd1ff9
4kmj54321 <4kmj54321@naver.com>
2025-07-28 13:16:11 +0900

    feat: 아티스트 전체 조회 API
    implement pagination for artist retrieval and add ArtistDTO for response

--- backend/src/main/java/com/a404/duckonback/service/ArtistService.java
+++ backend/src/main/java/com/a404/duckonback/service/ArtistService.java
@@ -1,8 +1,13 @@
 package com.a404.duckonback.service;
 
+import com.a404.duckonback.entity.Artist;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 
 public interface ArtistService {
     List<Integer> findAllArtistIdByUserId(Long id);
     void followArtists(Long id, List<Integer> artistList);
+    Page<Artist> getArtists(Pageable pageable);
 }
