6ea3bf1b47a307ab5fa36b14670b06edaba99e9c
4kmj54321 <4kmj54321@naver.com>
2025-07-29 15:20:56 +0900

    feat: 내 아티스트 팔로우 목록 조회 기능 구현

--- backend/src/main/java/com/a404/duckonback/service/ArtistFollowService.java
+++ backend/src/main/java/com/a404/duckonback/service/ArtistFollowService.java
@@ -1,6 +1,9 @@
 package com.a404.duckonback.service;
 
+import com.a404.duckonback.dto.FollowedArtistDTO;
 import com.a404.duckonback.entity.ArtistFollow;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +17,8 @@ public interface ArtistFollowService {
     boolean isFollowingArtist(Long id, Long artistId);
 
     void followArtists(Long id, List<Long> artistList);
+
+    Page<FollowedArtistDTO> getFollowedArtists(Long userId, Pageable pageable);
 
     void followArtist(Long userId, Long artistId);
     void unfollowArtist(Long userId, Long artistId);
