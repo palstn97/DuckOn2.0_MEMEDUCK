c942bb162a212ab4f18af39d6890c9eb5c2c9ff0
wnsqja7545 <wnsqja7545@gmail.com>
2025-07-29 15:34:17 +0900

    feat: 방 삭제 api 구현

--- backend/src/main/java/com/a404/duckonback/service/RedisService.java
+++ backend/src/main/java/com/a404/duckonback/service/RedisService.java
@@ -6,4 +6,5 @@ public interface RedisService {
     void saveRoomInfo(String roomId, LiveRoomDTO room);
     void addRoomToArtist(String artistId, String roomId);
     LiveRoomDTO getRoomInfo(String roomId);
+    void deleteRoomInfo(Long artistId, Long roomId);
 }
