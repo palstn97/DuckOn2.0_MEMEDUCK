82973323c7c3501c36d2dcf181fa11829f6149e4
Minsoo Park <palstn0624@gmail.com>
2025-08-03 12:09:56 +0900

    refactor: 비밀번호 확인 연동, mypage 상태 유지 오류 수정, 타 유저 페이지 수정

--- backend/FE/src/store/useUserStore.ts
+++ backend/FE/src/store/useUserStore.ts
@@ -4,15 +4,19 @@ import { persist, createJSONStorage } from 'zustand/middleware';
 import type { User } from '../types';
 
 type UserState = {
-  user: User | null;
-  setUser: (user: User | null) => void;
+  myUser: User | null // 내 정보
+  otherUser: User | null  // 타 유저 정보
+  setMyUser: (user: User | null) => void
+  setOtherUser: (user: User | null) => void
 };
 
 export const useUserStore = create<UserState>()(
   persist(
     (set) => ({
-      user: null,
-      setUser: (user) => set({ user }),
+      myUser: null,
+      otherUser: null,
+      setMyUser: (user) => set({ myUser: user }),
+      setOtherUser: (user) => set({ otherUser: user }),
     }),
     {
       name: 'user-storage',
