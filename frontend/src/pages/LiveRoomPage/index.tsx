8d10404710c06424da38ff7e41b9fd7a1ca31988
Minsoo Park <palstn0624@gmail.com>
2025-08-06 13:22:48 +0900

    refactor: 웹소켓 연결 시도

--- frontend/src/pages/LiveRoomPage/index.tsx
+++ frontend/src/pages/LiveRoomPage/index.tsx
@@ -2,7 +2,8 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { fetchRoomById } from "../../api/roomService";
 import { useUserStore } from "../../store/useUserStore";
-import { socket } from "../../socket";
+import { Client } from "@stomp/stompjs";
+import { createStompClient } from "../../socket";
 
 // 1. 페이지를 구성하는 자식 컴포넌트들을 import 합니다.
 import LiveHeader from "./LiveHeader";
@@ -15,8 +16,7 @@ const LiveRoomPage = () => {
   const myUserId = myUser?.userId;
   const [room, setRoom] = useState<any>(null);
   const navigate = useNavigate();
-  // const [videoId, setVideoId] = useState<string | null>(null)
-  // const [loading, setLoading] = useState(true)
+  const [stompClient, setStompClient] = useState<Client | null>(null)
 
   const handleExit = () => {
     navigate(-1);
@@ -25,7 +25,20 @@ const LiveRoomPage = () => {
   // 오른쪽 사이드바의 활성 탭 상태만 관리합니다.
   const [activeTab, setActiveTab] = useState<"chat" | "playlist">("chat");
 
-  const isHost = true;
+  const isHost = room?.hostId === myUserId
+  
+  useEffect(() => {
+    if (!myUser) return
+
+    const client = createStompClient(localStorage.getItem("accessToken") || "")
+    client.activate()
+    setStompClient(client)
+
+    return () => {
+      client.deactivate()
+    }
+  }, [myUser])
+
   useEffect(() => {
     const loadRoom = async () => {
       try {
@@ -40,7 +53,7 @@ const LiveRoomPage = () => {
     loadRoom();
   }, [roomId]);
 
-  if (!room) return <div>로딩 중...</div>;
+  if (!room || !stompClient || !myUser) return <div>로딩 중...</div>;
 
   return (
     // 전체 레이아웃
@@ -60,8 +73,10 @@ const LiveRoomPage = () => {
         <main className="flex-1 bg-black">
           <VideoPlayer
             videoId={room.playlist[room.currentVideoIndex]?.videoId}
-            isHost={room.hostId === myUserId}
-            socket={socket}
+            isHost={isHost}
+            stompClient={stompClient}
+            user={myUser!}
+            roomId={room.roomId}
           />
         </main>
 
@@ -91,11 +106,11 @@ const LiveRoomPage = () => {
             </button>
           </div>
 
-          <RightSidebar
+          {/* <RightSidebar
             selectedTab={activeTab}
             isHost={isHost}
             roomId={roomId}
-          />
+          /> */}
         </aside>
       </div>
     </div>
