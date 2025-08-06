c5858bd2612e2928cc70572d4b05c619463a66fb
mijin <mnmrtwg@gmail.com>
2025-08-06 13:05:31 +0900

    refactor: 비디오 카드 방장 정보 보이도록 수정

--- frontend/src/api/roomService.ts
+++ frontend/src/api/roomService.ts
@@ -1,17 +1,17 @@
 import { api } from "./axiosInstance";
-import type { Room } from "../types/Room";
+import type { room } from "../types/room";
 
 // 방 생성 API
 export const CreateRoom = async (formData: FormData) => {
-    const token = localStorage.getItem("accessToken")   // 개별 요청에서만 토큰 꺼내기
-    const response = await api.post("/api/rooms", formData, {
-        headers: {
-            // "Content-Type": "multipart/form-data",
-            ...(token && { Authorization: `Bearer ${token}` })  // 조건부로 헤더 추가
-        },
-    })
-    return response.data
-}
+  const token = localStorage.getItem("accessToken"); // 개별 요청에서만 토큰 꺼내기
+  const response = await api.post("/api/rooms", formData, {
+    headers: {
+      // "Content-Type": "multipart/form-data",
+      ...(token && { Authorization: `Bearer ${token}` }), // 조건부로 헤더 추가
+    },
+  });
+  return response.data;
+};
 
 // 방 정보 조회 API
 export const fetchRoomById = async (roomId: string) => {
@@ -29,7 +29,7 @@ export const fetchRoomById = async (roomId: string) => {
  * @param artistId - 조회할 아티스트의 ID
  * @returns 방 목록 배열
  */
-export const getRoomsByArtist = async (artistId: number): Promise<Room[]> => {
+export const getRoomsByArtist = async (artistId: number): Promise<room[]> => {
   // 지금 로그인 상태에서만 리스트가 불러와지네,,,,
   const token = localStorage.getItem("accessToken");
 
