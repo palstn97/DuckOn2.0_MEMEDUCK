33c30e6b104d719337c5ecb9db43b8e3c2f7245c
Minsoo Park <palstn0624@gmail.com>
2025-08-11 20:47:03 +0900

    feat: JWT 재발급

--- backend/FE/src/pages/OAuth2RedirectHandler.tsx
+++ backend/FE/src/pages/OAuth2RedirectHandler.tsx
@@ -2,7 +2,6 @@
 import { useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useUserStore } from "../store/useUserStore";
-import { api } from "../api/axiosInstance";
 import { getMyProfileAfterOAuth } from "../api/authService";
 import { emitTokenRefreshed } from "../api/axiosInstance";
 
