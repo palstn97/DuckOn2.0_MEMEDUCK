a30edd9715acf11b7905aa4f8a3e30fedcc1da17
Minsoo Park <palstn0624@gmail.com>
2025-08-16 16:26:19 +0900

    chore: 주석 제거

--- backend/FE/src/pages/LiveRoomPage/RightSidebar.tsx
+++ backend/FE/src/pages/LiveRoomPage/RightSidebar.tsx
@@ -1,54 +1,3 @@
-// import ChatPanel from "./ChatPanel";
-// import PlaylistPanel from "./PlaylistPanel";
-// import type { ChatMessage } from "../../types/chat";
-
-// type RightSidebarProps = {
-//   selectedTab: "chat" | "playlist";
-//   isHost: boolean;
-//   roomId: string | undefined;
-//   messages: ChatMessage[];
-//   sendMessage: (content: string) => void;
-//   playlist: string[];
-//   currentVideoIndex: number;
-//   onAddToPlaylist: (videoId: string) => void;
-//   onSelectPlaylistIndex?: (index: number) => void;
-//   onBlockUser: (userId: string) => void;
-// };
-
-// const RightSidebar = ({
-//   selectedTab,
-//   isHost,
-//   messages,
-//   sendMessage,
-//   playlist,
-//   currentVideoIndex,
-//   onAddToPlaylist,
-//   onSelectPlaylistIndex,
-//   onBlockUser,
-// }: RightSidebarProps) => {
-//   return (
-//     <div className="flex-grow flex flex-col bg-gray-800 min-h-0">
-//       {selectedTab === "chat" ? (
-//         <ChatPanel
-//           messages={messages}
-//           sendMessage={sendMessage}
-//           onBlockUser={onBlockUser}
-//         />
-//       ) : (
-//         <PlaylistPanel
-//           isHost={isHost}
-//           playlist={playlist}
-//           currentVideoIndex={currentVideoIndex}
-//           onAddToPlaylist={onAddToPlaylist}
-//           onSelect={onSelectPlaylistIndex}
-//         />
-//       )}
-//     </div>
-//   );
-// };
-
-// export default RightSidebar;
-
 import ChatPanel from "./ChatPanel";
 import PlaylistPanel from "./PlaylistPanel";
 import type { ChatMessage } from "../../types/chat";
@@ -64,8 +13,6 @@ type RightSidebarProps = {
   onAddToPlaylist: (videoId: string) => void;
   onSelectPlaylistIndex?: (index: number) => void;
   onBlockUser: (userId: string) => void;
-
-  // ★ 추가: 드래그 정렬 / 아이템 삭제
   onReorderPlaylist?: (from: number, to: number) => void;
   onDeletePlaylistItem?: (index: number) => void;
 };
@@ -81,8 +28,6 @@ const RightSidebar = ({
   onAddToPlaylist,
   onSelectPlaylistIndex,
   onBlockUser,
-
-  // ★ 추가된 콜백
   onReorderPlaylist,
   onDeletePlaylistItem,
 }: RightSidebarProps) => {
@@ -101,7 +46,6 @@ const RightSidebar = ({
           currentVideoIndex={currentVideoIndex}
           onAddToPlaylist={onAddToPlaylist}
           onSelect={onSelectPlaylistIndex}
-          // ★ 전달
           onReorder={onReorderPlaylist}
           onDeleteItem={onDeletePlaylistItem}
         />
