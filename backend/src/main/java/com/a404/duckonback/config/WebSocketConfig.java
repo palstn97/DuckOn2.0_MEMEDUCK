733346e0ab8b01ed4e985e3718d9b87bfc6fab98
wnsqja7545 <wnsqja7545@gmail.com>
2025-08-30 15:16:45 +0900

    [Refactor] 비로그인 사용자 방 입장 가능하게 변경 -> 재생정보 , 채팅 전송은 차단

--- backend/src/main/java/com/a404/duckonback/config/WebSocketConfig.java
+++ backend/src/main/java/com/a404/duckonback/config/WebSocketConfig.java
@@ -1,9 +1,11 @@
 package com.a404.duckonback.config;
 
 import com.a404.duckonback.interceptor.JwtHandshakeInterceptor;
+import com.a404.duckonback.interceptor.WsAccessInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -16,6 +18,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
+    private final WsAccessInterceptor wsAccessInterceptor;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -29,6 +32,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(wsAccessInterceptor);
     }
 
 
