24998fe8597600346cab02959123c81273e09be9
mijin <alwlsdlqslek3@gmail.com>
2025-10-09 18:38:48 +0900

    [Chore] eslint-plugin-better로 수정 (로직 변경 없음)

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,35 +1,39 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
-import prettierPlugin from 'eslint-plugin-prettier';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   {
-    ignores: ['dist', 'build', 'coverage', 'node_modules'],
+    ignores: ["dist", "build", "coverage", "node_modules"],
   },
   {
-    files: ['**/*.{ts,tsx,js,jsx}'],
+    files: ["**/*.{ts,tsx,js,jsx}"],
     languageOptions: {
       parser: tseslint.parser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: globals.browser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        React: "readonly",
+        YT: "readonly",
+      },
     },
     plugins: {
       react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'jsx-a11y': jsxA11y,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "jsx-a11y": jsxA11y,
       prettier: prettierPlugin,
-     '@typescript-eslint': tseslint.plugin, 
+      "@typescript-eslint": tseslint.plugin,
     },
     settings: {
-      react: { version: '18.2', runtime: 'automatic' },
+      react: { version: "18.2", runtime: "automatic" },
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -37,35 +41,32 @@ export default [
       ...reactHooks.configs.recommended.rules,
       ...eslintConfigPrettier.rules,
 
-      'prettier/prettier': 'warn',
-      semi: ['warn', 'always'],
+      "prettier/prettier": "warn",
+      semi: ["warn", "always"],
 
-      'arrow-body-style': 'off',
-      'prefer-arrow-callback': 'off',
+      "arrow-body-style": "off",
+      "prefer-arrow-callback": "off",
 
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-no-target-blank': 'off',
-      'react/prop-types': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-no-target-blank": "off",
+      "react/prop-types": "off",
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
 
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
         {
-          varsIgnorePattern: '^_',
-          argsIgnorePattern: '^_',
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
           ignoreRestSiblings: true,
         },
       ],
 
-      eqeqeq: ['error', 'always'],
+      eqeqeq: ["error", "always"],
 
-      'jsx-a11y/no-noninteractive-element-interactions': 'off',
-      'jsx-a11y/click-events-have-key-events': 'off',
-      'jsx-a11y/anchor-is-valid': 'off',
+      "jsx-a11y/no-noninteractive-element-interactions": "off",
+      "jsx-a11y/click-events-have-key-events": "off",
+      "jsx-a11y/anchor-is-valid": "off",
     },
   },
 ];
