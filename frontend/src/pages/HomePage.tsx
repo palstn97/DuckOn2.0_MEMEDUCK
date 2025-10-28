9c847d1843657aa1b569fc27446fadcb4f18f6c3
wnsqja7545 <wnsqja7545@gmail.com>
2025-10-28 10:16:58 +0900

    [S13P31A405-14[ infra: dev cicd test

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -181,7 +181,7 @@ const HomePage = () => {
         <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
           <div className="flex justify-between items-center mb-8">
             <h2 className="text-2xl md:text-3xl font-bold">
-              주목해야 할 아티스트!
+              주목해야 할 아티스트!!!!!!!
             </h2>
             <Link
               to="/artist-list"
