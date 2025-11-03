07ffef6d1bc9a45710a9b4dd2fa8a937f16b2263
wnsqja7545 <wnsqja7545@gmail.com>
2025-11-03 10:27:10 +0900

    [S13P31A406-86] feature: GIF 채팅 기능 구현

--- backend/src/main/java/com/a404/duckonback/dto/ChatMessageDTO.java
+++ backend/src/main/java/com/a404/duckonback/dto/ChatMessageDTO.java
@@ -16,7 +16,8 @@ public class ChatMessageDTO {
     private Long roomId;
     private String senderId;
     private String senderNickName;
-    private String content;
+    private String content; // isImage == true -> url / false -> String(일반 메시지)
+    private boolean isImage; // 해당 dto의 content가 이미지 url 인지, 일반 메세지(String) 인지
     @Builder.Default
     private Instant sentAt = Instant.now();
 }
