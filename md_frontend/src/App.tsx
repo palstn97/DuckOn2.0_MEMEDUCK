8e7f17135fc37de00488c71ee5368de491c6d2e4
박민수 <palstn97@naver.com>
2025-11-04 10:23:25 +0900

    feat: 밈덕 프론트엔드 기본적인 세팅

---
+++ md_frontend/src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
