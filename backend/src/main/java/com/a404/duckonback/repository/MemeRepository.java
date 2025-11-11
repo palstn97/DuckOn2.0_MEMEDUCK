c5cf862bdcc5131fb6450ac041a9cc093fdc88a1
wnsqja7545 <wnsqja7545@gmail.com>
2025-11-11 13:32:19 +0900

    [S13P31A406-157] refactor: import문 누락 해결

--- backend/src/main/java/com/a404/duckonback/repository/MemeRepository.java
+++ backend/src/main/java/com/a404/duckonback/repository/MemeRepository.java
@@ -1,12 +1,16 @@
 package com.a404.duckonback.repository;
 
+import com.a404.duckonback.dto.MyMemeDTO;
 import com.a404.duckonback.entity.Meme;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemeRepository extends JpaRepository<Meme, Long> {
     @Query("""
