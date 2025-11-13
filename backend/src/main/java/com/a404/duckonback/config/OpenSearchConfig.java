cd724e8a38fb551495c30381ef08c03e94ffccec
be8shyn <be8shyn@gmail.com>
2025-11-13 15:18:35 +0900

    [S13P31A406-204] fix : OpenSearch가 S3 자격 증명을 사용하도록 수정

--- backend/src/main/java/com/a404/duckonback/config/OpenSearchConfig.java
+++ backend/src/main/java/com/a404/duckonback/config/OpenSearchConfig.java
@@ -6,7 +6,8 @@ import org.opensearch.client.transport.aws.AwsSdk2TransportOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.http.SdkHttpClient;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.regions.Region;
@@ -19,14 +20,21 @@ public class OpenSearchConfig {
 
     @Value("${opensearch.region}")
     private String region;
+    
+    @Value("${S3_ACCESS_KEY}")
+    private String accessKey;
+    
+    @Value("${S3_SECRET_KEY}")
+    private String secretKey;
 
     @Bean
     public OpenSearchClient openSearchClient() {
         // SdkHttpClient 생성
         SdkHttpClient httpClient = ApacheHttpClient.builder().build();
 
-        // AWS 자격증명 프로바이더
-        DefaultCredentialsProvider credentialsProvider = DefaultCredentialsProvider.builder().build();
+        // AWS 자격증명 생성 (env.properties에서 읽어온 값 사용)
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+        StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(credentials);
 
         // AwsSdk2Transport 생성 - Region을 생성자에서 직접 전달
         AwsSdk2Transport transport = new AwsSdk2Transport(
